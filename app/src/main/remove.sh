2822515ef453760d13ad1294843fd9a329bd2aae
Peisheng YE <peisheng.ye.88@gmail.com>
2017-08-11 05:22:44 -0400

    first working BT chat code

---
+++ app/src/main/remove.sh
@@ -0,0 +1,7 @@
+FILE_LIST=`find -iname "*.java"`
+for each in $FILE_LIST
+do
+    echo your file is: $each
+    #sed -i -e 's/com.example.android.apis/com.radioyps.apidemos/g' $each
+    sed -i -e 's/com.example.android/com.radioyps/g' $each
+done
